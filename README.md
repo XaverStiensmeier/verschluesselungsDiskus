923f6e2e819b5cb62e49db274fab66ce987c00c9
Xaver Stiensmeier <xaverstiensmeier@gmx.de>
2025-01-10 14:54:34 +0100

    Polishing

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# verschluesselungsDiskus
-See https://asboran.de/wp-content/uploads/2017/11/Schwarzmaraskanischer-Verschl%C3%BCsselungsdiskus.pdf
+# Schwarzmariskanischer Diskus Verschluesserer
+
+This program implements a text encryption and decryption system based on a balanced binary tree structure (red/black).
+The encryption paths for each character are determined based on the tree structure (which is printed on creation), and the resulting encrypted text is visualized as SVG and PNG images.
+
+Take a look at Asboran who had the idea https://asboran.de/2015/10/16/spielhilfe-schwarzmaraskanischer-verschluesselungs-diskus/ and created a [pdf](https://asboran.de/wp-content/uploads/2017/11/Schwarzmaraskanischer-Verschl%C3%BCsselungsdiskus.pdf) describing how it works.
+
+## Running The Script
+
+To use the program, make sure you have Python 3 and the requirements installed (see requirements.txt). For installing the requirements I recommend setting up a [virtual environment](https://docs.python.org/3/library/venv.html).
+
+## EXE - Linux Only
+
+If you are using Linux you can use the provided exe in the `dist` folder. I need to compile the program for other operating systems on those operating systems. I could do windows. Please get in touch if you are interested.
+
+## How does it look like?
+### GUI
+First you need to create an encryption which is automatically loaded:
+
+![](img/gui.png)
+![](img/create.png)
+![](img/loaded.png)
+
+You can take a look at the tree (the image file is automatically generated). You can use that view to draw a nicer looking diskus for your group. Take a look at the [asboran version](https://asboran.de/wp-content/uploads/2015/10/Huffman-Scheibe-Haffax.png) to get an idea.
+
+![](img/encryption.png)
+
+Now you can click Select Text File To Encrypt and select a txt file:
+
+![](img/selectFile.png)
+
+The encrypted result is saved as an svg and png. Each word will be in a new line:
+
+![](img/encrypted.png)
+
+Now you are ready to include that png in whatever your handouts. Just make sure to give the players a chance to find the diskus - otherwise it is quite hard to crack for humans.
+
+## Future Readings
+
+If you are interested in the technical side of this topic, you might be interested in [Huffman Coding](https://en.wikipedia.org/wiki/Huffman_coding).
--- svd_gui.py
+++ svd_gui.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 import tkinter as tk
 from tkinter import filedialog
 from tkinter import messagebox
@@ -6,11 +8,24 @@ import os
 import pickle
 from xml.etree.ElementTree import Element, SubElement, tostring
 from xml.dom import minidom
+import graphviz
+import cairosvg
 
-NEW_LINE_PER_WORD = False
+current_encryption = None
+current_encryption_name = ""
+
+ALPHABET = "abcdefghijklmnopqrstuvwxyzäöüß.,"
+
+class BinaryTreeNode:
+    def __init__(self, char=None, left=None, right=None):
+        self.char = char
+        self.left = left
+        self.right = right
+
+def clean_string(input_string):
+    return ''.join([char for char in input_string if char in ALPHABET + " \n"])
 
 def create_svg(data):
-    # Set up SVG container with a minimal width and height, which will be dynamically adjusted
     svg = Element('svg', xmlns="http://www.w3.org/2000/svg", version="1.1")
     
     x_offset = 10  
@@ -20,10 +35,8 @@ def create_svg(data):
     circle_spacing = 15
     
     max_width = 0
-    max_height = 0
 
-    # Iterate through the data
-    for line in data:
+    for i, line in enumerate(data):
         if line == ' ': # newline for newword
             y_offset += line_height
             x_offset = 10
@@ -35,36 +48,34 @@ def create_svg(data):
             circle = SubElement(svg, 'circle', cx=str(x_offset), cy=str(y_offset), r=str(radius), fill=color)
             x_offset += circle_spacing  # Move to the next position horizontally
 
-        # Track the width and height to adjust SVG dimensions
         max_width = max(max_width, x_offset)
-        max_height = y_offset + line_height
-
-        if NEW_LINE_PER_WORD:
-            y_offset += line_height
-            x_offset = 10  # Reset horizontal position for the next line
 
     svg.set('width', str(max_width))
-    svg.set('height', str(max_height))
+    svg.set('height', str(y_offset+line_height/2))
 
     svg_str = tostring(svg, 'utf-8')
     parsed_svg = minidom.parseString(svg_str)
     return parsed_svg.toprettyxml(indent="  ")
 
-current_encryption = None
-current_encryption_name = ""
-
-ALPHABET = "abcdefghijklmnopqrstuvwxyzäöüß.!-"
-
-class BinaryTreeNode:
-    def __init__(self, char=None, left=None, right=None):
-        self.char = char
-        self.left = left
-        self.right = right
-
 def lame_log(number):
     for exp in range(10):
         if 2**exp >= number:
             return exp
+
+def visualize_tree(node, graph=None, parent=None, edge_color=None):
+    if graph is None:
+        graph = graphviz.Digraph(format='png', engine='dot')
+
+    if node is not None:
+        graph.node(str(id(node)), label=node.char if node.char and node.char in ALPHABET else "")
+        
+        if parent is not None:
+            graph.edge(str(id(parent)), str(id(node)), color=edge_color)
+        
+        visualize_tree(node.left, graph, node, edge_color='red')
+        visualize_tree(node.right, graph, node, edge_color='black')
+    
+    return graph
 
 def generate_balanced_binary_tree(alphabet):
     nodes = [BinaryTreeNode(char) for char in alphabet]
@@ -89,6 +100,11 @@ def generate_balanced_binary_tree(alphabet):
 def create_encryption():
     global current_encryption, current_encryption_name
     tree = generate_balanced_binary_tree(ALPHABET)
+
+    graph = visualize_tree(tree)
+    graph.render(f'{current_encryption_name}_binary_tree_drawing')
+    # graph.view(f'{current_encryption_name}_binary_tree_drawing')
+
     current_encryption = tree
     current_encryption_name = f"encryption_{random.randint(1000, 9999)}"  # Unique name for the encryption
     
@@ -96,29 +112,32 @@ def create_encryption():
     save_encryption(current_encryption, current_encryption_name)
     
     # Print the binary tree to the terminal
-    print("Generated Full Encryption Tree:")
-    print_tree(current_encryption)
+    # print("Generated Full Encryption Tree:")
+    # print_tree(current_encryption)
     
     # Update the current encryption label
     current_encryption_label.config(text=f"Current Encryption: {current_encryption_name}")
-    messagebox.showinfo("Encryption", f"New encryption created and saved as {current_encryption_name}.svd")
+    messagebox.showinfo("Encryption", f"New encryption created and saved as {current_encryption_name}.svdtree")
 
 def save_encryption(tree, name):
-    with open(f"{name}.svd", 'wb') as f:
+    with open(f"{name}.svdtree", 'wb') as f:
         pickle.dump(tree, f)
+
+    with open(f"{name}_alphabet.svd", "w") as f:
+        for char in ALPHABET:
+            f.write(f"{char}: {get_encryption_path(current_encryption, char).replace("0","r").replace("1", "b")}\n")
 
 def load_encryption():
     global current_encryption, current_encryption_name
-    filepath = filedialog.askopenfilename(filetypes=[("Encryption Files", "*.svd")])
+    filepath = filedialog.askopenfilename(filetypes=[("Encryption Files", "*.svdtree")])
     if filepath:
-        # Load the encryption tree from the file
         with open(filepath, 'rb') as f:
             current_encryption = pickle.load(f)
-        current_encryption_name = os.path.basename(filepath).replace(".svd", "")
+        current_encryption_name = os.path.basename(filepath).replace(".svdtree", "")
         
         # Print the loaded tree to the terminal
-        print(f"Loaded Encryption Tree from {current_encryption_name}:")
-        print_tree(current_encryption)
+        # print(f"Loaded Encryption Tree from {current_encryption_name}:")
+        # print_tree(current_encryption)
         
         current_encryption_label.config(text=f"Current Encryption: {current_encryption_name}")
 
@@ -141,9 +160,8 @@ def encrypt_file():
     if not filepath:
         return
     
-    # Read the file content
     with open(filepath, 'r', encoding='utf-8') as file:
-        text = file.read().lower()
+        text = clean_string(file.read().lower())
     
     # Generate encryption paths for each character
     encrypted_text = []
@@ -154,16 +172,15 @@ def encrypt_file():
         else:
             encrypted_text.append(' ')
 
-    save_encrypted_image(encrypted_text, os.path.basename(filepath))
+    save_encrypted_image(encrypted_text, os.path.basename(filepath), current_encryption_name)
 
-def save_encrypted_image(encrypted_text, original_filename):
-    # Create SVG
+def save_encrypted_image(encrypted_text, original_filename, current_encryption_name):
     svg_content = create_svg(encrypted_text)
-
-    # Save SVG to a file
-    with open(f"{original_filename}_encrypted.svg", "w") as f:
+    file_name = f"{original_filename}_encrypted_{current_encryption_name}"
+    with open(f"{file_name}.svg", "w") as f:
         f.write(svg_content)
-    messagebox.showinfo("Encryption", f"Successfully encrypted text to {original_filename}_encrypted.svg")
+    cairosvg.svg2png(url=f"{file_name}.svg", write_to=f"{file_name}.png")
+    messagebox.showinfo("Encryption", f"Successfully encrypted text to {original_filename}_encrypted_{current_encryption_name}")
 
 def print_tree(tree, level=0):
     if tree is not None:
@@ -175,7 +192,7 @@ def print_tree(tree, level=0):
         print_tree(tree.right, level + 1)
 
 root = tk.Tk()
-root.title("Schwarzmariskanischer Diskus Verschlüsserer")
+root.title("Schwarzmariskanischer Diskus Verschluesserer")
 
 tk.Button(root, text="Create Encryption", command=create_encryption).pack(pady=10)
 tk.Button(root, text="Load Encryption", command=load_encryption).pack(pady=10)
